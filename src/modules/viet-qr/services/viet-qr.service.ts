79daff318410f3887f74d37dd2ca9b95a33ec546
Dat Phan <phanminhdat2409@gmail.com>
2024-10-25 16:55:15 +0700

    vv

--- src/modules/viet-qr/services/viet-qr.service.ts
+++ src/modules/viet-qr/services/viet-qr.service.ts
@@ -134,7 +134,18 @@ export class VietQrService {
       return {
         message: 'Transaction processed successfully',
         refTransactionId,
-        id: body.transactionid,
+        transactionid: body.transactionid,
+        transactiontime: body.transactiontime,
+        referencenumber: body.referencenumber,
+        amount: body.amount,
+        content: body.content,
+        bankaccount: body.bankaccount,
+        orderId: body.orderId,
+        sign: body.sign,
+        terminalCode: body.terminalCode,
+        urlLink: body.urlLink,
+        serviceCode: body.serviceCode,
+        subTerminalCode: body.subTerminalCode,
       };
     } catch (error) {
       throw new BadRequestException({ status: true, message: error.message });
